2a24db709e4b2b18ce696c7f8afd85f0373d9411
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2021-03-23 23:16:20 +0100

    create empty game-options-form component

---
+++ ChessApp.Web/Components/game-options-form/game-options-form.base.cs
@@ -0,0 +1,14 @@
+﻿using ChessApp.Web.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChessApp.Web.Components
+{
+    public class GameOptionsFormBase: ComponentBase
+    {
+        protected GameOptions optionsModel { get; set; }
+    }
+}
