6f2700acec3445ecd61432bf82979031a7386533
tomaszgawron901 <mrgawron901@gmail.com>
2022-03-14 19:03:43 +0100

    remove unnecessary http client options from program.cs

--- ChessApp.Web/Program.cs
+++ ChessApp.Web/Program.cs
@@ -17,17 +17,14 @@ namespace ChessApp.Web
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+
             builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
             builder.Services.AddScoped<IStringLocalizer<App>, StringLocalizer<App>>();
 
-            builder.Services.AddHttpClient("chess_server_client", client => {
-                client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("chess_server_root"));
-            });
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<GameHubService>();
             builder.Services.AddScoped<JSInteropService>();
-
             builder.Services.AddTransient<GameRoomController>();
+
             await builder.Build().RunAsync();
         }
     }
